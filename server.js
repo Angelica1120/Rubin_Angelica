8ec7ff29e2305ceb453f96c7507ae05b966472ea
Rubin <rubinangelica21@gmail.com>
2025-11-18 09:53:01 +0800

    update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   res.send("Hello, Express is working!");
 });
 
-app.use("/api/user/1", userRoutes);
+app.use("/api/users/1", userRoutes);
 
 // Start the server
 app.listen(PORT, () => {
