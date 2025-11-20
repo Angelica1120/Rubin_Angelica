82423ae440ec2e77390ebcd67af6e8967e81cc45
Rubin <rubinangelica21@gmail.com>
2025-11-20 08:43:16 +0800

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,10 @@
 import users from "../models/userModel.js";
 
-export const getAllUser = () => {
+export const getAllUser = (req, res) => {
     try {
         res.send(users);
     }catch (error) {
         console.log("error" + error);
+        res.status(500).send("Server Error");
     }
 };
